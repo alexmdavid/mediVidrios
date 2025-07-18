0e0ed8988b4ef26ba00d0dc7523b88051a0f615c
alexdavid <joelarga2020@gmail.com>
2025-07-18 13:49:33 -0500

    agregando para solucion y manejo para vidrios asimetricos (sus lados miden diferente)

---
+++ src/main/java/com/datos/medividrios/model/VidriosAsimetricos.java
@@ -0,0 +1,15 @@
+package com.datos.medividrios.model;
+
+import com.datos.medividrios.enuum.TipoVidrio;
+
+public class VidriosAsimetricos {
+    private Float Lado1;
+    private Float Lado2;
+    private Float Lado3;
+    private Float Lado4;
+    private Integer espesor;
+    private Float precioM2;
+    private String color;
+    private TipoVidrio tipo;
+    private Artefacto artefacto;
+}
